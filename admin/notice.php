8fe9df8cd668e8a877949cea53f665b760105ab7
Ahmad Awais <mrahmadawais@gmail.com>
2016-06-27 05:50:18 +0500

    Docs Standards and function_exists
    It's a good practice to be on the safe side and use `function_exists` to check if no one else is adding the same function. One of the downsides of procedural programming even after you prefix them.

--- admin/notice.php
+++ admin/notice.php
@@ -1,95 +1,117 @@
 <?php
-
 /**
- *  Admin Notice to Encourage a Review or Donation
- *  
- *  @author Matt Cromwell
- *  @version 1.0
- *   
+ * Notice
+ *
+ * Notice related functionality goes in this file.
+ *
+ * @since   1.0.0
+ * @package WP
  */
 
-add_action( 'admin_notices', 'your_prefix_review_notice' );
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
-function your_prefix_review_notice() {
-	
-	// Define your Plugin name, review url, and donation url
-	$plugin_name = 'Test Plugin';
-    $review_url = 'https://wordpress.org/support/view/plugin-reviews/hello-dolly';
-    $donate_url = 'https://www.example.com/donate-here';
-	
-    //Get current user
-    global $current_user, $pagenow ;
-    $user_id = $current_user->ID;
+if ( ! function_exists( 'your_prefix_review_notice' ) ) {
+    // Add an admin notice.
+    add_action( 'admin_notices', 'your_prefix_review_notice' );
+    
+    /**
+     *  Admin Notice to Encourage a Review or Donation.
+     *
+     *  @author Matt Cromwell
+     *  @version 1.0.0
+     */
+    function your_prefix_review_notice() {
 
-	// Get today's timestamp
-    $today = mktime( 0, 0, 0, date("m")  , date("d"), date("Y") );
-    $actdate = get_option( 'your_prefix_activation_date', false );
+        // Define your Plugin name, review url, and donation url.
+        $plugin_name = 'Test Plugin';
+        $review_url = 'https://wordpress.org/support/view/plugin-reviews/hello-dolly';
+        $donate_url = 'https://www.example.com/donate-here';
 
-    $installed = ( !empty($actdate) ? $actdate : '999999999999999' );
+        // Get current user.
+        global $current_user, $pagenow ;
+        $user_id = $current_user->ID;
 
-    if ( $installed <= $today ) {
+        // Get today's timestamp.
+        $today = mktime( 0, 0, 0, date('m')  , date('d'), date('Y') );
+        $actdate = get_option( 'your_prefix_activation_date', false );
 
-        //Make sure we're on the plugins page.
-        if ( $pagenow == 'plugins.php' ) {
+        $installed = ( ! empty( $actdate ) ? $actdate : '999999999999999' );
 
-            // If the user hasn't already dismissed our alert,
-            // Output the activation banner
-            $nag_admin_dismiss_url = 'plugins.php?your_prefix_review_dismiss=0';
-            $user_meta             = get_user_meta( $user_id, 'your_prefix_review_dismiss' );
+        if ( $installed <= $today ) {
 
-            if ( empty($user_meta) ) {
+            // Make sure we're on the plugins page.
+            if ( 'plugins.php' == $pagenow ) {
 
-                ?>
-                <div class="notice notice-success">
+                // If the user hasn't already dismissed our alert,
+                // Output the activation banner.
+                $nag_admin_dismiss_url = 'plugins.php?your_prefix_review_dismiss=0';
+                $user_meta             = get_user_meta( $user_id, 'your_prefix_review_dismiss' );
 
-                    <style>
-                        p.review {
-                            position: relative;
-                            margin-left: 35px;
-                        }
-                        p.review span.dashicons-heart {
-                            color: white;
-                            background: #66BB6A;
-                            position: absolute;
-                            left: -50px;
-                            padding: 9px;
-                            top: -8px;
-                        }
+                if ( empty($user_meta) ) {
 
-                        p.review strong {
-                            color: #66BB6A;
-                        }
-                        p.review a.dismiss {
-                            float: right;
-                            text-decoration: none;
-                            color: #66BB6A;
-                        }
-                    </style>
-                    <?php
-                    // For testing purposes
-                    //echo '<p>Today = ' . $today . '</p>';
-                    //echo '<p>Installed = ' . $installed . '</p>';
                     ?>
+                    <div class="notice notice-success">
 
-                    <p class="review"><span class="dashicons dashicons-heart"></span><?php echo wp_kses( sprintf( __( 'Are you enjoying <strong>' . $plugin_name . '</strong>? Would you consider either a <a href="%1$s" target="_blank">small donation</a> or a <a href="%2$s" target="_blank">kind review to help continue development of this plugin?', 'your-textdomain' ), esc_url( $donate_url ), esc_url( $review_url ) ), array( 'strong' => array(), 'a' => array( 'href' => array(), 'target' => array() ) ) ); ?><a href="<?php echo admin_url( $nag_admin_dismiss_url ); ?>" class="dismiss"><span class="dashicons dashicons-dismiss"></span></a>
+                        <style>
+                            p.review {
+                                position: relative;
+                                margin-left: 35px;
+                            }
+                            p.review span.dashicons-heart {
+                                color: white;
+                                background: #66BB6A;
+                                position: absolute;
+                                left: -50px;
+                                padding: 9px;
+                                top: -8px;
+                            }
 
-                </div>
+                            p.review strong {
+                                color: #66BB6A;
+                            }
+                            p.review a.dismiss {
+                                float: right;
+                                text-decoration: none;
+                                color: #66BB6A;
+                            }
+                        </style>
+                        <?php
+                        // For testing purposes
+                        //echo '<p>Today = ' . $today . '</p>';
+                        //echo '<p>Installed = ' . $installed . '</p>';
+                        ?>
 
-            <?php }
+                        <p class="review"><span class="dashicons dashicons-heart"></span><?php echo wp_kses( sprintf( __( 'Are you enjoying <strong>' . $plugin_name . '</strong>? Would you consider either a <a href="%1$s" target="_blank">small donation</a> or a <a href="%2$s" target="_blank">kind review to help continue development of this plugin?', 'your-textdomain' ), esc_url( $donate_url ), esc_url( $review_url ) ), array( 'strong' => array(), 'a' => array( 'href' => array(), 'target' => array() ) ) ); ?><a href="<?php echo admin_url( $nag_admin_dismiss_url ); ?>" class="dismiss"><span class="dashicons dashicons-dismiss"></span></a>
+
+                    </div>
+
+                <?php }
+            }
         }
     }
 }
 
-// Function to force the Review Admin Notice to stay dismissed correctly
-add_action('admin_init', 'your_prefix_ignore_review_notice');
 
-function your_prefix_ignore_review_notice() {
-    if ( isset( $_GET[ 'your_prefix_review_dismiss' ] ) && '0' == $_GET[ 'your_prefix_review_dismiss' ] ) {
+if ( function_exists( 'your_prefix_ignore_review_notice' ) ) {
+    // Function to force the Review Admin Notice to stay dismissed correctly.
+    add_action('admin_init', 'your_prefix_ignore_review_notice');
 
-        //Get the global user
-        global $current_user;
-        $user_id = $current_user->ID;
+    /**
+     * Ignore review notice.
+     *
+     * @since  1.0.0
+     */
+    function your_prefix_ignore_review_notice() {
+        if ( isset( $_GET[ 'your_prefix_review_dismiss' ] ) && '0' == $_GET[ 'your_prefix_review_dismiss' ] ) {
 
-        add_user_meta( $user_id, 'your_prefix_review_dismiss', 'true', true );
+            // Get the global user.
+            global $current_user;
+            $user_id = $current_user->ID;
+
+            add_user_meta( $user_id, 'your_prefix_review_dismiss', 'true', true );
+        }
     }
 }
